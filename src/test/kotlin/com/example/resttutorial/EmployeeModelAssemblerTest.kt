5f6f495af41f3e63b9ed98afc125cd3922aed474
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2022-07-01 15:47:13 +0530

    add EmployeeModelAssemblerTest

---
+++ src/test/kotlin/com/example/resttutorial/EmployeeModelAssemblerTest.kt
@@ -0,0 +1,27 @@
+package com.example.resttutorial
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
+
+class EmployeeModelAssemblerTest {
+
+    @Test
+    fun transformsToEntityModel() {
+        val assembler = EmployeeModelAssembler()
+        val employee = Employee("Sam", "Don", "gardner")
+        employee.id = 100
+
+        val entity = assembler.toModel(employee)
+        assertThat(entity.content).isSameAs(employee)
+
+        val selfLink = linkTo(methodOn(EmployeeController::class.java).one(employee.id)).withSelfRel()
+        val employeesLink = linkTo(methodOn(EmployeeController::class.java).all()).withRel("employees")
+
+        assertThat(entity.links.contains(selfLink, employeesLink))
+            .withFailMessage("contains self and employees link")
+            .isTrue()
+    }
+
+}
