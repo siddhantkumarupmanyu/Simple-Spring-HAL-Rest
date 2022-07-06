d119061cf3683425bd1e1dcc95e37ce5d9a5753b
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2022-07-06 13:28:42 +0530

    EmployeeControllerTest.kt passing

--- src/test/kotlin/com/example/resttutorial/EmployeeControllerTest.kt
+++ src/test/kotlin/com/example/resttutorial/EmployeeControllerTest.kt
@@ -3,19 +3,18 @@ package com.example.resttutorial
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(EmployeeController::class)
+@Import(EmployeeModelAssembler::class)
 class EmployeeControllerTest {
-
 
     @MockBean
     private lateinit var repository: EmployeeRepository
@@ -29,8 +28,10 @@ class EmployeeControllerTest {
             .andDo(MockMvcResultHandlers.print())
             .andExpect(status().isOk)
             .andExpect(jsonPath("$._links.self.href").value("http://localhost/employees"))
-        // .andExpect(jsonPath("$._embedded").doesNotExist())
+            .andExpect(jsonPath("$._embedded").doesNotExist())
 
         verify(repository).findAll()
     }
 }
+
+// https://stackoverflow.com/a/64486074
