28d57ac72258dd832594cd29fbcaa53453a4c647
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2022-07-07 13:06:23 +0530

    rename EmployeeJpaRepo.kt to EmployeeJpaWrapper.kt

--- src/main/kotlin/com/example/resttutorial/repository/EmployeeJpaWrapper.kt
+++ src/main/kotlin/com/example/resttutorial/repository/EmployeeJpaWrapper.kt
@@ -3,25 +3,6 @@ package com.example.resttutorial.repository
 import com.example.resttutorial.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 
-
-// tbh i don't find myself comfortable around extending interfaces
-// i feel interfaces are abstract things and then there are impl. of those.
-// with extending interfaces i am creating an unneeded coupling
-// and most importantly hierarchy
-// which not only hinders understandability and readability
-// but makes it harder to makes sense of code.
-// no i need to navigate between multiple interfaces
-// someone would say this is just DRY but
-// tbh imo, this is abuse of DRY
-
-// i hate interface hierarchy
-// another reason i hate them is cause they make the testing harder.
-// now i need to like fake/mock everything by JpaRepository... :(
-// and another issue is i don't own that interface... :(
-
-// should be auto implemented by spring, and injected in EmployeeJpaWrapper
-interface EmployeeJpaRepo : JpaRepository<Employee, Long> {}
-
 // todo:
 // impl.
 // i haven't made this @Component so,
@@ -58,3 +39,4 @@ class EmployeeJpaWrapper(
 
 }
 
+interface EmployeeJpaRepo : JpaRepository<Employee, Long> {}
